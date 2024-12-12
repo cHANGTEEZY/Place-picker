da6b8f30403a3ce0424e2a02b45df0a479f94fdc
changteezy <jagatgurung30@gmail.com>
2024-12-12 23:45:24 +0545

    edited for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,48 @@
 import fs from "node:fs/promises";
-
+import path from "node:path";
 import bodyParser from "body-parser";
 import express from "express";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static("images"));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 
-// CORS
-
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
   next();
 });
 
 app.get("/places", async (req, res) => {
   const fileContent = await fs.readFile("./data/places.json");
-
   const placesData = JSON.parse(fileContent);
-
   res.status(200).json({ places: placesData });
 });
 
 app.get("/user-places", async (req, res) => {
   const fileContent = await fs.readFile("./data/user-places.json");
-
   const places = JSON.parse(fileContent);
-
   res.status(200).json({ places });
 });
 
 app.put("/user-places", async (req, res) => {
   const places = req.body.places;
-
   await fs.writeFile("./data/user-places.json", JSON.stringify(places));
-
   res.status(200).json({ message: "User places updated!" });
 });
 
-// 404
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    return next();
-  }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.use((req, res) => {
   res.status(404).json({ message: "404 - Not Found" });
 });
 
