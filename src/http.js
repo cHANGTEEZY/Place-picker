d4732b80f1bc961c0116baf6cf9dbcd9374b8b96
changteezy <jagatgurung30@gmail.com>
2024-12-13 00:51:08 +0545

    minor changes

--- src/http.js
+++ src/http.js
@@ -1,38 +1,48 @@
 export async function fetchAvailablePlaces() {
-  const response = await fetch('http://localhost:3000/places');
+  const response = await fetch(
+    "http://localhost:3000/places" ||
+      "https://place-picker-taupe.vercel.app/places"
+  );
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error('Failed to fetch places');
+    throw new Error("Failed to fetch places");
   }
 
   return resData.places;
 }
 
 export async function fetchUserPlaces() {
-  const response = await fetch('http://localhost:3000/user-places');
+  const response = await fetch(
+    "http://localhost:3000/user-places" ||
+      "https://place-picker-taupe.vercel.app/user-places"
+  );
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error('Failed to fetch user places');
+    throw new Error("Failed to fetch user places");
   }
 
   return resData.places;
 }
 
 export async function updateUserPlaces(places) {
-  const response = await fetch('http://localhost:3000/user-places', {
-    method: 'PUT',
-    body: JSON.stringify({ places }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const response = await fetch(
+    "http://localhost:3000/user-places" ||
+      "https://place-picker-taupe.vercel.app/user-places",
+    {
+      method: "PUT",
+      body: JSON.stringify({ places }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error('Failed to update user data.');
+    throw new Error("Failed to update user data.");
   }
 
   return resData.message;
